335799e1186aec0385dcd192923048d0d09227a8
A S Md Ferdousul Haque <ferdousul.haque@portonics.com>
2021-10-07 23:21:28 +0600

    Functional test fixed

--- tests/HateeHoTest.php
+++ tests/HateeHoTest.php
@@ -2,9 +2,10 @@
 
 namespace Tests;
 
-use App\Helpers\isGreaterThan;
-use App\Helpers\isOneOf;
-use App\Helpers\isDivisibleBy;
+use App\Matcher\GreaterThan;
+use App\Matcher\InArray;
+use App\Matcher\DivisibleBy;
+use App\StringGenerator\Config;
 use App\Response;
 use PHPUnit\Framework\TestCase;
 
@@ -29,8 +30,8 @@ class HateeHoTest extends TestCase
     public function testHateeHo($input, $conditions, $append, $expectation): void
     {
         $result = $this->responder
-            ->input($input)
-            ->conditions($conditions)
+            ->setInput($input)
+            ->setConfigs($conditions)
             ->append($append)
             ->response();
         $this->assertEquals($expectation, $result);
@@ -44,19 +45,19 @@ class HateeHoTest extends TestCase
     {
         return [
             array([1, 20], [
-                ["logic" => [new isDivisibleBy(3), new isDivisibleBy(5)], "print" => "papow"],
-                ["logic" => [new isDivisibleBy(3)], "print" => "pa"],
-                ["logic" => [new isDivisibleBy(5)], "print" => "pow"],
+                new Config([new DivisibleBy(3), new DivisibleBy(5)], 'papow'),
+                new Config([new DivisibleBy(3)], 'pa'),
+                new Config([new DivisibleBy(5)], 'pow'),
             ], " ", "1 2 pa 4 pow pa 7 8 pa pow 11 pa 13 14 papow 16 17 pa 19 pow"),
             array([1, 15], [
-                ["logic" => [new isDivisibleBy(2), new isDivisibleBy(7)], "print" => "hateeho"],
-                ["logic" => [new isDivisibleBy(7)], "print" => "ho"],
-                ["logic" => [new isDivisibleBy(2)], "print" => "hatee"],
+                new Config([new DivisibleBy(2), new DivisibleBy(7)], 'hateeho'),
+                new Config([new DivisibleBy(7)], 'ho'),
+                new Config([new DivisibleBy(2)], 'hatee'),
             ], "-", "1-hatee-3-hatee-5-hatee-ho-hatee-9-hatee-11-hatee-13-hateeho-15"),
             array([1, 10], [
-                ["logic" => [new isOneOf([1, 4, 9]), new isGreaterThan(5)], "print" => "jofftchoff"],
-                ["logic" => [new isOneOf([1, 4, 9])], "print" => "joff"],
-                ["logic" => [new isGreaterThan(5)], "print" => "tchoff"]
+                new Config([new InArray([1, 4, 9]), new GreaterThan(5)], 'jofftchoff'),
+                new Config([new InArray([1, 4, 9])], 'joff'),
+                new Config([new GreaterThan(5)], 'tchoff'),
             ], "-", "joff-2-3-joff-5-tchoff-tchoff-tchoff-jofftchoff-tchoff"),
         ];
     }
